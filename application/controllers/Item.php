541878c0be829f4fe02e7a4a8e4371da0c048af8
Jocelio Lima <jocelio27@gmail.com>
2015-09-20 16:14:11 -0300

    #6 Adding layout template of project, #5 continuing items crud

--- application/controllers/Item.php
+++ application/controllers/Item.php
@@ -6,50 +6,98 @@ class Item extends CI_Controller {
 	public function __construct(){
 		parent::__construct();
 		$this->load->helper(array('url','form','array'));
-		$this->load->library('form_validation');
+		$this->load->library(array('form_validation','session'));
 		$this->load->database();
     }
 	 
 	public function index(){
 		
-		$this->load->view('item', array(
-					'part' => 'search'
+		$this->load->view('index', array(
+					'page'=>'item'
+					,'title'=> 'Itens'
+					,'part' => 'searching'
 					,'tabledata'=>$this->db->get('item')->result()
 				));
 	}
 	
-	public function search(){
+	public function searching(){
 		$this->db->like('descricao', $this->input->get('descricao'));
 		
-		$this->load->view('item',array(
-					'part' => 'search'
+		$this->load->view('index',array(
+					'page'=>'item'
+					,'title'=> 'Itens'
+					,'part' => 'searching'
 					,'tabledata'=>$this->db->get('item')->result()
 				));
 	}
 	
-	public function insert(){
-		$this->load->view('item', array('part' => 'insert'));
+	public function inserting(){
+		$this->load->view('index', array(
+					'page'=>'item'
+					,'title'=> 'Itens'
+					,'part' => 'inserting'));
 	}
 	
+	public function editing(){
+		$id = $this->uri->segment(3) ? $this->uri->segment(3) : $this->input->post('id_item');
+		if($id){
+			$this->load->view('index', array(
+						'page'=>'item'
+						,'title'=> 'Itens'
+						,'part' => 'editing'
+						,'item'=> $this->db->get_where('item', array('id_item' => $id))->row() ));
+		}else{
+			$this->searching();
+		}
+	}
 	
 	public function save(){
-		$this->form_validation->set_message('monetary',"%s não corresponde a um padrão de moeda.");
-		$this->form_validation->set_rules('descricao', 'Descrição', 'trim|required|min_length[5]|max_length[60]|ucwords');
-		$this->form_validation->set_rules('preco', 'Preço', 'required');
-		
-		if ($this->form_validation->run() == TRUE){
+		if ($this->runFormValidations() == TRUE){
 			
 			$dados = elements(array('descricao','preco'),$this->input->post());
 			$this->db->insert('item', $dados); 
 			
-			$this->load->view('item',array(
-					'msg' => 'Item cadastrado com sucesso'
-					,'part' => 'insert'
-				));
+			$this->load->view('index',array(
+					'page'=>'item'
+					,'title'=> 'Itens'
+					,'part' => 'inserting'
+			));
+			
+			$this->session->set_flashdata('msg', 'Item cadastrado com sucesso.');
+			redirect(current_url());
 			
 		}else{
-			$this->load->view('item', array('part' => 'insert'));
+			$this->inserting();
 		}
+	}
+	
+	public function edit(){
+		if ($this->runFormValidations() == TRUE){
+			
+			$dados = elements(array('descricao','preco'),$this->input->post());
+			
+			$this->db->where('id_item', $this->input->post('id_item'));
+			$this->db->update('item', $dados); 
+			
+			$this->session->set_flashdata('msg', 'Item atualizado com sucesso.');
+			redirect(current_url());
+			
+			
+		}else{
+			$this->editing();
+		}
+	}
+	
+	private function runFormValidations(){
+	
+		$this->form_validation->set_message('monetary',"%s não corresponde a um padrão de moeda.");
+		
+		$this->form_validation->set_message('descricao',"%s é um campo obrigatório.");
+		$this->form_validation->set_rules('descricao', 'Descrição', 'trim|required|min_length[5]|max_length[60]|ucwords');
+		$this->form_validation->set_rules('preco', 'Preço', 'required');
+		
+		return $this->form_validation->run();
+	
 	}
 	
 	public function monetary($str){
---
+++ application/views/pages/item.php
@@ -0,0 +1,149 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+ 
+?>
+<script>
+	$(document).ready(function(){
+		$('#preco').mask('000.000.000.000.000,00', {reverse: true});
+	});
+</script>
+
+<?php 
+/**
+* Área da tela responsável pela pesquisa e exibição da lista de resultados
+*/
+	if($part =="searching"){
+?>
+
+	<form action="<?php echo site_url();?>/item/searching">
+	<div class="row">
+		<div class="col-md-5 form-group">
+			<input type="text" placeholder="Descrição do item" name="descricao" class="form-control"/>
+		</div>
+		<div class="col-md-5 form-group">
+			<input type="submit" name="submit" value="Buscar" class="btn btn-sucess">
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-1 col-often-11 form-group pull-right">
+			<a class="btn btn-info" href="<?php echo site_url();?>/item/inserting">Novo</a>
+		</div>
+	</div>
+	</form>
+	<div class="row">
+		<div class="large-12 columns">
+		<table class="table table-responsive"> 
+			<tr>
+				<th>ID</th>
+				<th>Descrição</th>
+				<th>Preço</th>
+				<th>Opções</th>
+			</tr>
+			<?php foreach($tabledata as $item){ ?>
+			<tr>
+				<td><?php echo $item->id_item ?></td>
+				<td width="70%"><?php echo $item->descricao ?></td>
+				<td><?php echo $item->preco ?></td>
+				<td>
+					<a href="<?php echo site_url();?>/item/editing/<?php  echo $item->id_item ?>">Editar 
+						<span class="glyphicon glyphicon-edit">
+						</span>
+					</a>
+				</td>
+			</tr>
+			<?php } ?>
+		</table> 
+		</div>
+	</div>
+	
+<?php 
+/**
+* Área da tela responsável pelo formulário de inserção de dados
+*/
+	}else if($part =="inserting"){
+		
+	echo form_open('item/save');	
+?>
+
+<div class="row">
+	<div class="col-md-6 form-group">		  
+	  <?php
+		echo form_label('Descrição');
+		echo form_input(array('name'=>'descricao','class'=>'form-control','placeholder'=>'Descrição do item'),set_value('descricao'),'autofocus');
+	  ?>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-6 form-group">
+	  <?php
+		echo form_label('Preço');
+		echo form_input(array('name'=>'preco','id'=>'preco','class'=>'form-control','placeholder'=>'Preço do item'),set_value('preco'));
+	  ?>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-6 form-group">
+	 <?php
+		echo form_submit(array('name'=>'cadastrar','class' =>'btn btn-success'),'Cadastrar')." ";
+		echo form_reset(array('name'=>'limpar','class' =>'btn btn-danger'),'Limpar');
+	  ?>
+	
+	</div>
+	<div class="col-md-6 form-group">
+		<a class="btn btn-info" href="<?php echo site_url();?>/item" class="button success">Voltar</a>  
+	</div>
+</div>
+
+<?php 
+echo form_close();
+
+/**
+* Área da tela responsável pelo formulário de edição de dados
+*/
+	}else if($part =="editing"){
+		
+	echo form_open('item/edit');
+	echo form_hidden('id_item', $item->id_item);
+?>
+
+<div class="row">
+	<div class="col-md-6 form-group">		  
+	  <?php
+		echo form_label('Descrição');
+		echo form_input(array('name'=>'descricao','class'=>'form-control','placeholder'=>'Descrição do item'),$item->descricao ,'autofocus');
+	  ?>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-6 form-group">
+	  <?php
+		echo form_label('Preço');
+		echo form_input(array('name'=>'preco','id'=>'preco','class'=>'form-control','placeholder'=>'Preço do item'),$item->preco);
+	  ?>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-6 form-group">
+	 <?php
+		echo form_submit(array('name'=>'editar','class' =>'btn btn-success'),'Editar')." ";
+		echo form_reset(array('name'=>'limpar','class' =>'btn btn-danger'),'Limpar');
+	  ?>	
+	</div>
+	<div class="col-md-6 form-group">
+		<a class="btn btn-info" href="<?php echo site_url();?>/item" class="button success">Voltar</a>  
+	</div>
+</div>
+
+<?php
+}
+?>
+
+<div class="row">
+	<?php echo validation_errors(); ?>
+	
+	<?php if(!empty($this->session->flashdata('msg'))){ ?>
+	<div class="alert alert-success">
+	  <?php echo $this->session->flashdata('msg'); ?>	
+	</div>
+	<?php } ?>
+</div>	
