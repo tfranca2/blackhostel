d9ea1047e81a7189946d3d362bf128258211452e
Jocelio Lima <jocelio27@gmail.com>
2015-10-12 21:13:34 -0300

    db dump

--- blackhostel.sql
+++ blackhostel.sql
@@ -1,197 +1,116 @@
--- phpMyAdmin SQL Dump
--- version 4.4.12
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: 10-Out-2015 às 17:49
--- Versão do servidor: 5.6.25
--- PHP Version: 5.5.27
+# Host: localhost  (Version: 5.6.25)
+# Date: 2015-10-12 21:11:55
+# Generator: MySQL-Front 5.3  (Build 4.234)
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+/*!40101 SET NAMES latin1 */;
 
+#
+# Structure for table "cliente"
+#
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `blackhostel`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `cliente`
---
-
-CREATE TABLE IF NOT EXISTS `cliente` (
-  `id_cliente` int(11) NOT NULL,
+DROP TABLE IF EXISTS `cliente`;
+CREATE TABLE `cliente` (
+  `id_cliente` int(11) NOT NULL AUTO_INCREMENT,
   `cliente` varchar(100) NOT NULL,
   `cpf` varchar(15) NOT NULL,
-  `rg` varchar(20) NOT NULL
+  `rg` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_cliente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+#
+# Structure for table "item"
+#
 
---
--- Estrutura da tabela `item`
---
-
-CREATE TABLE IF NOT EXISTS `item` (
-  `id_item` int(11) NOT NULL,
+DROP TABLE IF EXISTS `item`;
+CREATE TABLE `item` (
+  `id_item` int(11) NOT NULL AUTO_INCREMENT,
   `descricao` varchar(200) NOT NULL,
-  `preco` decimal(10,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `preco` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id_item`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+#
+# Structure for table "perfil"
+#
 
---
--- Estrutura da tabela `perfil`
---
-
-CREATE TABLE IF NOT EXISTS `perfil` (
-  `id_perfil` int(11) NOT NULL,
+DROP TABLE IF EXISTS `perfil`;
+CREATE TABLE `perfil` (
+  `id_perfil` int(11) NOT NULL AUTO_INCREMENT,
   `descricao` varchar(200) NOT NULL,
   `preco_base` decimal(10,2) NOT NULL,
-  `tp_modo_reserva` int(2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `tp_modo_reserva` int(2) NOT NULL,
+  PRIMARY KEY (`id_perfil`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+#
+# Structure for table "perfil_item"
+#
 
---
--- Estrutura da tabela `perfil_item`
---
-
-CREATE TABLE IF NOT EXISTS `perfil_item` (
+DROP TABLE IF EXISTS `perfil_item`;
+CREATE TABLE `perfil_item` (
   `id_perfil` int(11) NOT NULL,
   `id_item` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+#
+# Structure for table "produto"
+#
 
---
--- Estrutura da tabela `produto`
---
-
-CREATE TABLE IF NOT EXISTS `produto` (
-  `id_produto` int(11) NOT NULL,
+DROP TABLE IF EXISTS `produto`;
+CREATE TABLE `produto` (
+  `id_produto` int(11) NOT NULL AUTO_INCREMENT,
   `produto` varchar(100) NOT NULL,
-  `preco` decimal(10,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `preco` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id_produto`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+#
+# Structure for table "quarto"
+#
 
---
--- Estrutura da tabela `quarto`
---
-
-CREATE TABLE IF NOT EXISTS `quarto` (
-  `id_quarto` int(10) NOT NULL,
+DROP TABLE IF EXISTS `quarto`;
+CREATE TABLE `quarto` (
+  `id_quarto` int(10) NOT NULL AUTO_INCREMENT,
   `numero` varchar(255) NOT NULL,
   `descricao` varchar(255) DEFAULT NULL,
-  `id_perfil` int(10) NOT NULL
+  `id_perfil` int(10) NOT NULL,
+  PRIMARY KEY (`id_quarto`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+#
+# Structure for table "reserva"
+#
+
+DROP TABLE IF EXISTS `reserva`;
+CREATE TABLE `reserva` (
+  `id_reserva` int(11) NOT NULL AUTO_INCREMENT,
+  `id_quarto` int(11) NOT NULL,
+  `entrada` datetime NOT NULL,
+  `saida` datetime DEFAULT NULL,
+  `situacao` int(11) NOT NULL,
+  `qt_pessoas` int(11) NOT NULL,
+  PRIMARY KEY (`id_reserva`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+#
+# Structure for table "situacao"
+#
+
+DROP TABLE IF EXISTS `situacao`;
+CREATE TABLE `situacao` (
+  `id_situacao` int(11) NOT NULL AUTO_INCREMENT,
+  `situacao` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_situacao`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+#
+# Structure for table "usuario"
+#
 
---
--- Estrutura da tabela `situacao`
---
-
-CREATE TABLE IF NOT EXISTS `situacao` (
-  `id_situacao` int(11) NOT NULL,
-  `situacao` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `usuario`
---
-
-CREATE TABLE IF NOT EXISTS `usuario` (
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE `usuario` (
   `id_usuario` int(11) NOT NULL,
   `nome` varchar(255) NOT NULL,
   `login` varchar(255) NOT NULL,
   `senha` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `cliente`
---
-ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`id_cliente`);
-
---
--- Indexes for table `item`
---
-ALTER TABLE `item`
-  ADD PRIMARY KEY (`id_item`);
-
---
--- Indexes for table `perfil`
---
-ALTER TABLE `perfil`
-  ADD PRIMARY KEY (`id_perfil`);
-
---
--- Indexes for table `produto`
---
-ALTER TABLE `produto`
-  ADD PRIMARY KEY (`id_produto`);
-
---
--- Indexes for table `quarto`
---
-ALTER TABLE `quarto`
-  ADD PRIMARY KEY (`id_quarto`);
-
---
--- Indexes for table `situacao`
---
-ALTER TABLE `situacao`
-  ADD PRIMARY KEY (`id_situacao`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `cliente`
---
-ALTER TABLE `cliente`
-  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `item`
---
-ALTER TABLE `item`
-  MODIFY `id_item` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `perfil`
---
-ALTER TABLE `perfil`
-  MODIFY `id_perfil` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `produto`
---
-ALTER TABLE `produto`
-  MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `quarto`
---
-ALTER TABLE `quarto`
-  MODIFY `id_quarto` int(10) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `situacao`
---
-ALTER TABLE `situacao`
-  MODIFY `id_situacao` int(11) NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
